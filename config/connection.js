b3cc4c5b078281db076d26307c8873a1107e2a19
John Garrett <infantrygarrett@icloud.com>
2019-04-12 14:03:54 -0500

    updated connection with jaws

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,32 @@
+// const mysql = require("mysql");
+
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 8889,
+//     user: "root",
+//     password: "root",
+//     database: "burgers_db"
+// })
+
+// module.exports = connection
+
+//make the connection
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "followme",
-    database: "burgers_db"
-})
+//EDIT THIS IF SOMETHING ISNT WORKING BC USUALLY THIS IS THE PROBLEM!!!!
+let connection;
+   if (process.env.JAWSDB_URL){
+       connection = mysql.createConnection(process.env.JAWSDB_URL);
+    } else {
+        connection = mysql.createConnection({
+            host: "localhost",
+            port: 8889, 
+            user: "root",
+            password: "root",
+            database: "burgers_db"
+        });
+    };
+    
 
+//export
 module.exports = connection
